0d0ba9923199e004125b6632ddb05349bc258733
Petra12345 <78352813+Petra12345@users.noreply.github.com>
2023-01-31 12:02:57 +0100

    add test and begin P test

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,39 +1,68 @@
 from scr.functions import *
 import matplotlib.pyplot as plt
 
-# load data
-xi_train, tau_train, xi_test, tau_test = load_data(n_train=100)
+# # load data
+# xi_train, tau_train, xi_test, tau_test = load_data(n_train=800, n_test=200)
 
-# perform SGD
-w, errors = sgd_training(xi_train=xi_train, tau_train=tau_train, n_epochs=500)
+# # perform SGD
+# errors = []
+# test_errors = []
+# print("----------- train and test network -------------")
+# for i in range(10):
+#     print("---------- run ", i, "---------------")
+#     w, err, test_err = sgd_training(xi_train=xi_train, tau_train=tau_train, xi_test=xi_test,tau_test=tau_test, n_epochs=10000)
+#     errors.append(err)
+#     test_errors.append(test_err)
 
-plt.plot(errors)
-plt.title("Training error")
-plt.xlabel("Epochs")
-plt.ylabel("Error")
-plt.legend(["Training error", "Test error"])
-plt.show()
+# mean_train_error = np.mean(errors, 0)
+# mean_test_error = np.mean(test_errors,0)
 
-plt.bar(np.arange(w[:,0].size), w[:,0])
-plt.title("Components of w1 vector")
-plt.xlabel("Component")
-plt.ylabel("Value")
-plt.show()
 
-plt.bar(np.arange(w[:,1].size), w[:,1])
-plt.title("Components of w2 vector")
-plt.xlabel("Component")
-plt.ylabel("Value")
-plt.show()
+# print('----------------- plot -----------------')
+# plt.plot(mean_train_error, label='Training error')
+# plt.plot(mean_test_error, label='Test error')
+# plt.legend()
+# plt.title("Training error")
+# plt.xlabel("Epochs")
+# plt.ylabel("Error") 
+# plt.show()
 
-sigma_test = calculate_output(k=np.shape(w)[1], xi=xi_test, w=w)
-plt.plot(tau_test, sigma_test, 'bo')
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('Test set prediction')
-plt.show()
-mse = np.mean((sigma_test - tau_test) ** 2)
-print("Mean squared error: ", mse)
+print('-------------- Test different Ps -------------------')
+P = [20, 50, 200, 500, 1000, 2000]
+train_errors = []
+test_errors=[]
+for n_train in P:
+    print('---------------- P = ', n_train, ' ------------------')
+    xi_train, tau_train, xi_test, tau_test = load_data(n_train=n_train, n_test=200)
+    w, err, test_err = sgd_training(xi_train=xi_train, tau_train=tau_train, xi_test=xi_test,
+                                    tau_test=tau_test, n_epochs=10000)
+    train_errors.append(err)
+    test_errors.append(test_err)
+    
+plt.plot(train_errors)
+plt.legend(['P=20', 'P=50', 'P=50'])
+    
+
+# plt.bar(np.arange(w[:,0].size), w[:,0])
+# plt.title("Components of w1 vector")
+# plt.xlabel("Component")
+# plt.ylabel("Value")
+# plt.show()
+
+# plt.bar(np.arange(w[:,1].size), w[:,1])
+# plt.title("Components of w2 vector")
+# plt.xlabel("Component")
+# plt.ylabel("Value")
+# plt.show()
+
+# sigma_test = calculate_output(k=np.shape(w)[1], xi=xi_test, w=w)
+# plt.plot(tau_test, sigma_test, 'bo')
+# plt.xlabel('True values')
+# plt.ylabel('Predicted values')
+# plt.title('Test set prediction')
+# plt.show()
+# mse = np.mean((sigma_test - tau_test) ** 2)
+# print("Mean squared error: ", mse)
 
 
 
--- assignment3/scr/functions.py
+++ assignment3/scr/functions.py
@@ -5,26 +5,26 @@ import random
 
 # function to load the data
 #TODO: add n_test??
-def load_data(n_train=5000):
+def load_data(n_train=100, n_test=100):
     # n_data gives the number of instances to use
-    xi = pd.read_csv("../data/xi.csv")
-    tau = pd.read_csv("../data/tau.csv")
+    xi = pd.read_csv("./data/xi.csv")
+    tau = pd.read_csv("./data/tau.csv", header=None, index_col=False)
     xi = xi.to_numpy()
-    tau = np.array(list(tau))
+    tau = tau.to_numpy()
     xi_train = xi[:,0:n_train]
-    tau_train = tau[0:n_train]
-    xi_test = xi[:,n_train:]
-    tau_test = tau[n_train:]
-
+    tau_train = tau[:,0:n_train]
+    xi_test = xi[:,n_train:n_train+n_test]
+    tau_test = tau[:,n_train:n_train+n_test]
     return xi_train, tau_train, xi_test, tau_test
 
 
 ###################### Function for SGD ############################
 
 # initialize weight vectors with L2-norm of 1
-def initialize_weights(k, N):
+def initialize_weights(k, N, seed=0):
     w = np.zeros([N, k])
     for i in range(k):
+        np.random.seed(i+seed)
         w_k = np.random.randint(0, 100, N)
         w_unit = w_k/np.linalg.norm(w_k,2)
         w[:,i] = w_unit
@@ -37,7 +37,14 @@ def calculate_output(k, xi, w):
     return sigma
 
 def calculate_error(sigma, tau):
-    return np.mean(1/2 * (sigma - tau.astype(float)) ** 2)
+    return np.mean(1/2 * (sigma - tau) ** 2)
+
+def calculate_test_error(k, xi, tau, w):
+    sigma = []
+    for i in range(np.shape(xi)[1]):
+        output = calculate_output(k, xi[:,i], w)
+        sigma.append(output)
+    return calculate_error(sigma, tau)
 
 def calculate_gradient(xi,sigma, tau, w):
     gradient = (sigma - tau) * (1 - np.tanh(w.T @ xi.reshape(-1, 1))**2)
@@ -47,7 +54,7 @@ def calculate_learning_step(xi_train, tau_train, w, learning_rate):
     # select random example
     idx = random.randint(0, len(xi_train))
     xi = xi_train[:, idx]
-    tau = np.asarray(tau_train[idx], dtype=float)    
+    tau = tau_train[0,idx]   
     
     # calculate sigma
     sigma = calculate_output(k=np.shape(w)[1], xi=xi, w=w)
@@ -55,24 +62,29 @@ def calculate_learning_step(xi_train, tau_train, w, learning_rate):
     # calculate error
     error = calculate_error(sigma, tau)
 
-    print("sigma: ", sigma, " tau: ", tau, " error: ", error)
+    #print("sigma: ", sigma, " tau: ", tau, " error: ", error)
 
     # calculate gradient
     partial_gradients = calculate_gradient(xi, sigma, tau, w)
-
     # update weights 
     for i in range(len(partial_gradients)):
         g = partial_gradients[i] * xi
-        w[:,i] -= learning_rate * g * error
+        w[:,i] -= learning_rate * g * error    # *error?
     return w
 
-def sgd_training(xi_train, tau_train, n_epochs=50, learning_rate=0.05, k=2):
+def sgd_training(xi_train, tau_train, xi_test, tau_test, n_epochs=500, learning_rate=0.05, seed=0, k=2):
     # initialize weights
-    w = initialize_weights(k, len(xi_train))
+    w = initialize_weights(k, len(xi_train), seed)
     errors = []
+    test_errors = []
     for i in range(n_epochs):
         w = calculate_learning_step(xi_train=xi_train, tau_train=tau_train, w=w, learning_rate=learning_rate)
-        sigma_train = calculate_output(k=np.shape(w)[1], xi=xi_train, w=w)
-        error_train = calculate_error(sigma_train, tau_train)
-        errors.append(error_train)
-    return w, errors
+        # calculate errors each P steps
+        if i % np.shape(tau_train)[1] == 0:
+            sigma_train = calculate_output(k=2, xi=xi_train, w=w)
+            error_train = calculate_error(sigma_train, tau_train)
+            errors.append(error_train)
+            sigma_test = calculate_output(k=2, xi=xi_test, w=w)
+            error = calculate_error(sigma_test, tau_test)
+            test_errors.append(error)
+    return w, errors, test_errors
